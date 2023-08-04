9eb729f907b468ab46f115cc37b6f130c90c26e8
Hussen Mohamed <hussen88gamer88@gmail.com>
2023-08-05 00:19:29 +0300

    HTML and CSS and JavaScript files

---
+++ script.js
@@ -0,0 +1,5 @@
+const numberButtons = document.querySelectorAll(`[data-number]`);
+const operationButtons =document.querySelectorAll(`[data-operation]`);
+const equalsButton = document.querySelector(`[data-equals]`);
+const allClearButton = document.querySelector(`[data-all-clear]`);
+const deleteButton = document.querySelector(`[data-delete]`);
