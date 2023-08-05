b492ae4ec48b5a40cf96a6e53819a63c8c148846
unknown <hussen88gamer88@gmail.com>
2023-08-05 13:47:39 +0100

    Finisged the Project

--- script.js
+++ script.js
@@ -17,8 +17,9 @@ class Calculator{
         this.currentOperand = this.currentOperand.toString().slice(0, -1);
     }
 
+    // when user clicks on number button this function will be called
     appendNumber(number) {
-        //
+        //if the number user wants to input is '.' and there is already a dot in the current operand so return nothing
         if(number === '.' && this.currentOperand.includes('.')) {
             return;
         }
@@ -26,7 +27,7 @@ class Calculator{
     }
 
     chooseOperation(operation) {
-        //
+        //when the user clicks on operation button without inputting any numbers
         if (this.currentOperand === '') {
             return;
         }
@@ -54,7 +55,7 @@ class Calculator{
             return;
         }
 
-        //
+        // Mathematics Operations
         switch(this.operation) {
             case '+':
                 computation = prev + current;
@@ -73,22 +74,54 @@ class Calculator{
 
         }
 
-        //
+        // The result of our computation will appear in the current operand section
         this.currentOperand = computation;
+
+        // reset the operation and previous operand
         this.operation = undefined;
         this.previousOperand = '';
     }
 
+    // formatting and displaying number in a user-friendly way 
+    getDisplayNumber(number) {
+        // convert the number to string to use the (split) function
+        const stringNumber = number.toString();
+
+        //get the left part of the number before the dot '.' => integer
+        const integerDigits = parseFloat(stringNumber.split('.')[0]);
+
+        //get the right part of the number after the dot '.' => decimal
+        const decimalDigits = stringNumber.split('.')[1];
+
+        // the string version of integerDigits
+        let integerDisplay;
+        if(isNaN(integerDigits)) {
+            integerDisplay = '';
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en' , {
+                maximumFractionDigits: 0
+            })
+        }
+
+        // seperate between integer digits and decimal digits by a dot '.'
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`;
+        } else {
+            return `${integerDisplay}`;
+        }
+    }
 
     updateDisplay() {
-        this.currentOperandText.innerText = this.currentOperand;
+        this.currentOperandText.innerText = this.getDisplayNumber(this.currentOperand);
         if (this.operation != null){
-            this.previousOperandText.innerText = this.previousOperand + ' '  + this.operation;
+            this.previousOperandText.innerText = this.getDisplayNumber(this.previousOperand) + ' '  + this.operation;
+        } else {
+            this.previousOperandText.innerText = '';
         }
     }
 }
 
-
+// SELECTORS
 const numberButtons = document.querySelectorAll(`[data-number]`);
 const operationButtons =document.querySelectorAll(`[data-operation]`);
 const equalsButton = document.querySelector(`[data-equals]`);
@@ -99,7 +132,7 @@ const currentOperandText = document.querySelector(`[data-current-operand]`);
 
 const calculator = new Calculator(previousOperandText,currentOperandText);
 
-//
+// Numbers and dot '.'
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
         calculator.appendNumber(button.innerText);
@@ -107,7 +140,7 @@ numberButtons.forEach(button => {
     })
 });
 
-//
+// Operations
 operationButtons.forEach(button => {
     button.addEventListener('click', () => {
         calculator.chooseOperation(button.innerText);
@@ -115,19 +148,19 @@ operationButtons.forEach(button => {
     })
 });
 
-//
+// = Button
 equalsButton.addEventListener('click', button => {
     calculator.compute();
     calculator.updateDisplay();
 });
 
-//
+// AC Button
 allClearButton.addEventListener('click', button => {
     calculator.clear();
     calculator.updateDisplay();
 });
 
-//
+// DEL Button
 deleteButton.addEventListener('click', button => {
     calculator.delete();
     calculator.updateDisplay();
